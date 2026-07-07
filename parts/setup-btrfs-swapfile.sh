282e5c4a6e616a8d4d22fd7b5854b5090601d60e
runt <>
2026-07-07 22:15:07 +0900

    setup qemu and update swapfile

--- parts/setup-btrfs-swapfile.sh
+++ parts/setup-btrfs-swapfile.sh
@@ -1,9 +1,26 @@
 #!/bin/bash
 set -euo pipefail
 
+mem_total_kb=$(grep MemTotal /proc/meminfo | awk '{print $2}')
+default_size_gib=$(((mem_total_kb + 1048575) / 1048576))
+
+read -p "Enter swap file size in GiB [Default: ${default_size_gib}]: " user_input
+
+if [ -z "${user_input}" ]; then
+  swap_size_gib=${default_size_gib}
+elif [[ ! "${user_input}" =~ ^[0-9]+$ ]] || [ "${user_input}" -le 0 ]; then
+  echo "Error: Invalid size. Please enter a positive integer." >&2
+  exit 1
+else
+  swap_size_gib=$((user_input))
+fi
+
+echo "Setting up a ${swap_size_gib}GiB swapfile..."
+
 swapon --show | grep -q '/swapfile' && sudo swapoff /swapfile
 sudo rm /swapfile
-sudo btrfs filesystem mkswapfile --size 32g /swapfile
+sudo btrfs filesystem mkswapfile --size "${swap_size_gib}g" /swapfile
 sudo swapon /swapfile
+grep --quiet '/swapfile' /etc/fstab || echo "/swapfile none swap defaults 0 0" | sudo tee --append /etc/fstab >/dev/null
 swapon --show
 free -h
---
+++ parts/setup-qemu-kvm-libvirt.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -euo pipefail
+
+# setup vm env with qemu and kvm
+sudo apt update
+sudo apt install qemu-system virt-manager libvirt-daemon-system
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+NC='\033[0m'
+echo
+echo -e "${GREEN}Click on to launch ${RED}virt-manager${GREEN} and proceed with the installation as you would with VMware or VirtualBox.${NC}"
+echo
+echo -e "${RED}mount -t virtiofs src dst${GREEN} - allows you to use the virtiofs filesystem driver to mount the host shared folder to the guest PC.${NC}"
+echo
+echo -e "${GREEN}For advanced disk management without launching virt-manager, such as for mounting VHDX/QCOW2 directly, you may ${RED}apt install qemu-utils${GREEN} and run ${RED}qemu-nbd${GREEN} or run ${RED}qemu-img${GREEN} for editing image files.${NC}"
