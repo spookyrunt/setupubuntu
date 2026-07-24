88127dcfb2bad57c99de0868df0b2e519f604f8d
runt <>
2026-07-24 21:17:25 +0900

    add setup wine

--- parts/setup-wine.sh
+++ parts/setup-wine.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -euo pipefail
 
+sudo dpkg --add-architecture i386
 sudo apt update -y
-sudo apt install -y wine winetricks winbind
+sudo apt install -y wine winetricks winbind \
+  wine32 wine64 wine64-tools wine64-preloader wine32-preloader wine-binfmt
 winecfg
