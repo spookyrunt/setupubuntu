4796e38c2392d60a8f760bd4d2c1e2e0daf5619b
runt <>
2026-07-26 02:05:31 +0900

    winetricks

--- parts/setup-wine.sh
+++ parts/setup-wine.sh
@@ -5,4 +5,5 @@ sudo dpkg --add-architecture i386
 sudo apt update -y
 sudo apt install -y wine winetricks winbind \
   wine32 wine64 wine64-tools wine64-preloader wine32-preloader wine-binfmt
+winetricks -q corefonts cjkfonts dxvk d3dcompiler_47 d3dcompiler_43 dotnet48 vcrun2022 vcrun2013 mfc42
 winecfg
