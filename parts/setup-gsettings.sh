dbfef630e961dc8e010311cba642fe49ca385768
spookyrunt <283396452+spookyrunt@users.noreply.github.com>
2026-09-18 14:52:04 +0900

    Update gsettings for power and color preferences
    Added additional gsettings configurations for power management and color scheme.

--- parts/setup-gsettings.sh
+++ parts/setup-gsettings.sh
@@ -4,10 +4,14 @@ set -euo pipefail
 gsettings set org.gnome.desktop.interface text-scaling-factor 1.10
 gsettings set org.gnome.desktop.interface monospace-font-name 'JetBrainsMono Nerd Font 12'
 gsettings set org.gnome.SessionManager logout-prompt false
-gsettings set org.gnome.shell.extensions.dash-to-dock dock-position 'RIGHT' || true
-gsettings set org.gnome.shell.extensions.dash-to-dock show-mounts-only-mounted true || true
+gsettings set org.gnome.shell.extensions.dash-to-dock dock-position 'RIGHT'
+gsettings set org.gnome.shell.extensions.dash-to-dock show-mounts-only-mounted true
 # gsettings set org.gnome.mutter center-new-windows false
 gsettings set org.gnome.mutter attach-modal-dialogs false
 gsettings set org.gnome.desktop.screensaver lock-enabled false
 gsettings set org.gnome.desktop.screensaver lock-delay 0
 gsettings set org.gnome.desktop.session idle-delay 900
+gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-ac-type 'nothing'
+gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-ac-timeout 0
+gsettings set org.gnome.settings-daemon.plugins.color night-light-enabled true
+gsettings set org.gnome.desktop.interface color-scheme 'prefer-light'
